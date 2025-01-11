2fe48e7c74fec6c38f51751366e5358303249996
newtsarthur <tutuhoficial13@gmail.com>
2025-01-11 13:59:03 -0300

    TaskManager implementações

--- taskmanager/src/main/java/TaskManager.java
+++ taskmanager/src/main/java/TaskManager.java
@@ -1,4 +1,31 @@
 package taskmanager.src.main.java;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class TaskManager {
+    private List<Task> tasks = new ArrayList<>();
+    private int nextId = 1;
+
+    public void createTask(Scanner scanner) {
+        System.out.print("Digite a descrição da tarefa: ");
+        String description = scanner.nextLine();
+        Task task = new Task(nextId++, description);
+        tasks.add(task);
+        System.out.println("Tarefa concluída com sucesso!");
+    }
+
+    public void listTasks() {
+        if(tasks.isEmpty()) {
+            System.out.println("Nenhuma tarefa encontrada.");
+        } else {
+            System.out.println("\n --- Lista de tarefas ---");
+            for (Task task : tasks) {
+                System.out.println(task);
+            }
+        }
+    }
+
+
 }
