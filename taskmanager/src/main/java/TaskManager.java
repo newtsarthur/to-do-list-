e0405bf8c9db70bfb411d5607a57d821a4d650be
newtsarthur <tutuhoficial13@gmail.com>
2025-01-11 14:51:06 -0300

    Completada a classe TaskManager

--- taskmanager/src/main/java/TaskManager.java
+++ taskmanager/src/main/java/TaskManager.java
@@ -27,5 +27,45 @@ public class TaskManager {
         }
     }
 
+    public void editTask(Scanner scanner) {
+        System.out.print("Digite o ID da tarefa que deseja editar: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        Task task = findTaskById(id);
 
+        if(task != null) {
+            System.out.print("Nova descrição: ");
+            String newDescription = scanner.nextLine();
+            task.setDescription(newDescription);
+
+            System.out.print("A tarefa está concluída? (s/n) : ");
+            String completed = scanner.nextLine();
+            task.setCompleted(completed.equalsIgnoreCase("s"));
+            System.out.println("Tarefa atuliazada com sucesso!");
+        } else {
+            System.out.println("Tarefa não encontrada!");
+        }
+    }
+
+    public void deleteTask(Scanner scanner) {
+        System.out.print("Digite o ID da tarefa que quer deletar: ");
+        int id = scanner.nextInt();
+        Task task = findTaskById(id);
+
+        if(task != null) {
+            tasks.remove(task);
+            System.out.println("Tarefa excluída com sucesso!");
+        } else {
+            System.out.println("Tarefa não encontrada!");
+        }
+    }
+
+    private Task findTaskById(int id) {
+        for (Task task : tasks) {
+            if(task.getId() == id) {
+                return task;
+            }
+        }
+        return null;
+    }
 }
